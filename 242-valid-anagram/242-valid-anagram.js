d881db24a803d5c07b73f85f4ff815f251223548
Sherline Au <au.sherline@gmail.com>
2022-08-07 12:59:54 -0500

    Time: 101 ms (78.53%), Space: 44.1 MB (56.23%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.js
+++ 242-valid-anagram/242-valid-anagram.js
@@ -4,6 +4,9 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
+    if (s.length !== t.length) return false;
+    
+    //create a hash map of first string
     let hash = {}
     for (let letter of s)
         {
@@ -15,23 +18,31 @@ var isAnagram = function(s, t) {
                 hash[letter]++
             }
         }
+    
+    // iterate through second string 
     for (let letter of t)
         {
+            // if a letter is found that is not in the hash map, return false
             if(!hash[letter])
                 {
                     return false
                 }
+            // otherwise decrement the value 
             else
                 {
                     hash[letter]--
                 }
         }
+    
+    // check the hash map
     for (let obj in hash)
         {
+            // if any letter in the hash map is not zero, return false
             if(hash[obj] !== 0)
                 {
                     return false
                 }
         }
+    // return true at end of function -> since false is returned if any extra letter is found or the hashmap is not "empty"
     return true
 };
